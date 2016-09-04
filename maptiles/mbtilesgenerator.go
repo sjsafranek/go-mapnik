91d59800ebf707bc2113dae8e025eb76659194d4
stefan <stefan@stefan.stefan>
2016-09-04 10:19:52 -0700

    refactor to file mbtile schema

--- maptiles/mbtilesgenerator.go
+++ maptiles/mbtilesgenerator.go
@@ -1,15 +1,15 @@
 package maptiles
 
 import (
-	"crypto/md5"
+	// "crypto/md5"
 	"database/sql"
-	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
-	//"net/http"
-	//"strconv"
 )
+
+var logger *log.Logger = log.New(os.Stdout, "[TILE] ", log.LUTC|log.Ldate|log.Ltime|log.Lshortfile|log.Lmicroseconds)
 
 // MBTiles 1.2-compatible Tile Db with multi-layer support.
 // Was named Mbtiles before, hence the use of *m in methods.
@@ -26,16 +26,14 @@ func NewTileDb(path string) *TileDb {
 	var err error
 	m.db, err = sql.Open("sqlite3", path)
 	if err != nil {
-		log.Println("Error opening db", err.Error())
+		logger.Println("Error opening db", err.Error())
 		return nil
 	}
 	queries := []string{
 		"PRAGMA journal_mode = OFF",
-		"CREATE TABLE IF NOT EXISTS layers(layer_name text PRIMARY KEY NOT NULL)",
-		"CREATE TABLE IF NOT EXISTS metadata (name text PRIMARY KEY NOT NULL, value text NOT NULL)",
-		"CREATE TABLE IF NOT EXISTS tiles (layer_id integer, zoom_level integer, tile_column integer, tile_row integer, tile_data blob, checksum text, PRIMARY KEY (layer_id, zoom_level, tile_column, tile_row) FOREIGN KEY(checksum) REFERENCES tile_blobs(checksum))",
-		"CREATE TABLE IF NOT EXISTS tile_blobs (checksum text, tile_data blob)",
-		"CREATE VIEW IF NOT EXISTS tiles AS SELECT tiles.zoom_level as zoom_level, tiles.tile_column as tile_column, tiles.tile_row as tile_row, (SELECT tile_data FROM tile_blobs WHERE checksum=tiles.checksum) as tile_data FROM tiles WHERE tiles.layer_id = (SELECT rowid FROM layers WHERE layer_name='default')",
+		"CREATE TABLE IF NOT EXISTS layers(layer_name TEXT PRIMARY KEY NOT NULL)",
+		"CREATE TABLE IF NOT EXISTS metadata (name TEXT PRIMARY KEY NOT NULL, value TEXT NOT NULL)",
+		"CREATE TABLE IF NOT EXISTS tiles (layer_id INTEGER, zoom_level INTEGER, tile_column INTEGER, tile_row INTEGER, tile_data blob, PRIMARY KEY (layer_id, zoom_level, tile_column, tile_row))",
 		"REPLACE INTO metadata VALUES('name', 'go-mapnik cache file')",
 		"REPLACE INTO metadata VALUES('type', 'overlay')", //baselayer
 		"REPLACE INTO metadata VALUES('version', '1')",
@@ -49,7 +47,8 @@ func NewTileDb(path string) *TileDb {
 	for _, query := range queries {
 		_, err = m.db.Exec(query)
 		if err != nil {
-			log.Println("Error setting up db", err.Error())
+			logger.Println("Error setting up db", err.Error())
+			logger.Println(query, "\n");
 			return nil
 		}
 	}
@@ -62,104 +61,109 @@ func NewTileDb(path string) *TileDb {
 	return &m
 }
 
-func (m *TileDb) readLayers() {
-	m.layerIds = make(map[string]int)
-	rows, err := m.db.Query("SELECT rowid, layer_name FROM layers")
+func (self *TileDb) readLayers() {
+	self.layerIds = make(map[string]int)
+	rows, err := self.db.Query("SELECT rowid, layer_name FROM layers")
 	if err != nil {
-		log.Fatal("Error fetching layer definitions", err.Error())
+		logger.Fatal("Error fetching layer definitions", err.Error())
 	}
 	var s string
 	var i int
 	for rows.Next() {
 		if err := rows.Scan(&i, &s); err != nil {
-			log.Fatal(err)
+			logger.Fatal(err)
 		}
-		m.layerIds[s] = i
+		self.layerIds[s] = i
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 }
 
-func (m *TileDb) ensureLayer(layer string) {
-	if _, ok := m.layerIds[layer]; !ok {
-		if _, err := m.db.Exec("INSERT OR IGNORE INTO layers(layer_name) VALUES(?)", layer); err != nil {
-			log.Println(err)
+func (self *TileDb) ensureLayer(layer string) {
+	if _, ok := self.layerIds[layer]; !ok {
+		if _, err := self.db.Exec("INSERT OR IGNORE INTO layers(layer_name) VALUES(?)", layer); err != nil {
+			logger.Println(err)
 		}
-		m.readLayers()
+		self.readLayers()
 	}
 }
 
-func (m *TileDb) Close() {
-	close(m.insertChan)
-	close(m.requestChan)
-	if m.qc != nil {
-		<-m.qc // block until channel qc is closed (meaning Run() is finished)
+func (self *TileDb) Close() {
+	close(self.insertChan)
+	close(self.requestChan)
+	if self.qc != nil {
+		<-self.qc // block until channel qc is closed (meaning Run() is finished)
 	}
-	if err := m.db.Close(); err != nil {
-		log.Print(err)
+	if err := self.db.Close(); err != nil {
+		logger.Print(err)
 	}
 
 }
 
-func (m TileDb) InsertQueue() chan<- TileFetchResult {
-	return m.insertChan
+func (self TileDb) InsertQueue() chan<- TileFetchResult {
+	return self.insertChan
 }
 
-func (m TileDb) RequestQueue() chan<- TileFetchRequest {
-	return m.requestChan
+func (self TileDb) RequestQueue() chan<- TileFetchRequest {
+	return self.requestChan
 }
 
 // Best executed in a dedicated go routine.
-func (m *TileDb) Run() {
-	m.qc = make(chan bool)
+func (self *TileDb) Run() {
+	self.qc = make(chan bool)
 	for {
 		select {
-		case r := <-m.requestChan:
-			m.fetch(r)
-		case i := <-m.insertChan:
-			m.insert(i)
+		case r := <-self.requestChan:
+			self.fetch(r)
+		case i := <-self.insertChan:
+			self.insert(i)
 		}
 	}
-	m.qc <- true
+	self.qc <- true
 }
 
-func (m *TileDb) insert(i TileFetchResult) {
+func (self *TileDb) insert(i TileFetchResult) {
 	i.Coord.setTMS(true)
-	x, y, z, l := i.Coord.X, i.Coord.Y, i.Coord.Zoom, i.Coord.Layer
+	x, y, zoom, l := i.Coord.X, i.Coord.Y, i.Coord.Zoom, i.Coord.Layer
 	if l == "" {
 		l = "default"
 	}
-	h := md5.New()
-	_, err := h.Write(i.BlobPNG)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	s := fmt.Sprintf("%x", h.Sum(nil))
-	row := m.db.QueryRow("SELECT 1 FROM tile_blobs WHERE checksum=?", s)
+	// h := md5.New()
+	// _, err := h.Write(i.BlobPNG)
+	// if err != nil {
+	// 	logger.Println(err)
+	// 	return
+	// }
+	// s := fmt.Sprintf("%x", h.Sum(nil))
+	// row := self.db.QueryRow("SELECT 1 FROM tile_blobs WHERE checksum=?", s)
+	queryString := "SELECT tile_data FROM tiles WHERE layer_id=? AND zoom_level=? AND tile_column=? AND tile_row=?"
+	row := self.db.QueryRow(queryString, self.layerIds[l], zoom, x, y)
 	var dummy uint64
-	err = row.Scan(&dummy)
+	err := row.Scan(&dummy)
 	switch {
 	case err == sql.ErrNoRows:
-		if _, err = m.db.Exec("REPLACE INTO tile_blobs VALUES(?,?)", s, i.BlobPNG); err != nil {
-			log.Println("error during insert", err)
+		// queryString = "REPLACE INTO tiles(tile_data) VALUES(?) WHERE layer_id=? AND zoom_level=? AND tile_column=? AND tile_row=?"
+		queryString = "UPDATE tiles SET tile_data=? WHERE layer_id=? AND zoom_level=? AND tile_column=? AND tile_row=?"
+		if _, err = self.db.Exec(queryString, i.BlobPNG, self.layerIds[l], zoom, x, y); err != nil {
+			logger.Println("error during insert", err)
 			return
 		}
 	case err != nil:
-		log.Println("error during test", err)
+		logger.Println("error during test", err)
 		return
 	default:
-		//log.Println("Reusing blob", s)
+		logger.Println("Reusing blob", self.layerIds[l], zoom, x, y)
 	}
-	m.ensureLayer(l)
-	sql := "REPLACE INTO tiles VALUES(?, ?, ?, ?, ?, ?)"
-	if _, err = m.db.Exec(sql, m.layerIds[l], z, x, y, i.BlobPNG, s); err != nil {
-		log.Println(err)
+	self.ensureLayer(l)
+	// queryString = "REPLACE INTO tiles VALUES(?, ?, ?, ?, ?)"
+	queryString = "REPLACE INTO tiles VALUES(?, ?, ?, ?, ?)"
+	if _, err = self.db.Exec(queryString, self.layerIds[l], zoom, x, y, i.BlobPNG); err != nil {
+		logger.Println(err)
 	}
 }
 
-func (m *TileDb) fetch(r TileFetchRequest) {
+func (self *TileDb) fetch(r TileFetchRequest) {
 	r.Coord.setTMS(true)
 	zoom, x, y, l := r.Coord.Zoom, r.Coord.X, r.Coord.Y, r.Coord.Layer
 	if l == "" {
@@ -168,45 +172,35 @@ func (m *TileDb) fetch(r TileFetchRequest) {
 	result := TileFetchResult{r.Coord, nil}
 	queryString := `
 		SELECT tile_data 
-		FROM tile_blobs 
-		WHERE checksum=(
-			SELECT checksum 
-			FROM tiles 
-			WHERE zoom_level=? 
-				AND tile_column=? 
-				AND tile_row=?
-				AND layer_id=(SELECT rowid FROM layers WHERE layer_name=?)
-		)`
+		FROM tiles 
+		WHERE zoom_level=? 
+			AND tile_column=? 
+			AND tile_row=?
+			AND layer_id=?
+		`
 	var blob []byte
-	row := m.db.QueryRow(queryString, zoom, x, y, l)
+	row := self.db.QueryRow(queryString, zoom, x, y, self.layerIds[l])
 	err := row.Scan(&blob)
 	switch {
 	case err == sql.ErrNoRows:
 		result.BlobPNG = nil
 	case err != nil:
-		log.Println(err)
+		logger.Println(err)
 	default:
 		result.BlobPNG = blob
 	}
 	r.OutChan <- result
 }
 
-
-func (m *TileDb) MetaDataHandler() map[string]string {
-
-	rows, _ := m.db.Query("SELECT * FROM metadata")
-
+// gets tile data
+func (self *TileDb) MetaDataHandler() map[string]string {
+	rows, _ := self.db.Query("SELECT * FROM metadata")
 	metadata :=  make(map[string]string)
-
 	for rows.Next() {
-
 		var name string
 		var value string
 		rows.Scan(&name, &value)
-		
 		metadata[name] = value
 	}
-
 	return metadata
-
 }
