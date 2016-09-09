69988533bd57cb1b9f7c88c4686e5286e011a7e2
stefan <stefan@stefan.stefan>
2016-09-08 22:58:28 -0700

    postgres mbtile support

---
+++ demo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"./mapnik"
+)
+
+// Render a simple map of europe to a PNG file
+func SimpleExample(map_file string) {
+	m := mapnik.NewMap(1600, 1200)
+	defer m.Free()
+	m.Load(map_file)
+	fmt.Println(m.SRS())
+	// Perform a projection that is only neccessary because stylesheet.xml
+	// is using EPSG:3857 rather than WGS84
+	p := m.Projection()
+	ll := p.Forward(mapnik.Coord{0, 35})  // 0 degrees longitude, 35 degrees north
+	ur := p.Forward(mapnik.Coord{16, 70}) // 16 degrees east, 70 degrees north
+	m.ZoomToMinMax(ll.X, ll.Y, ur.X, ur.Y)
+	blob, err := m.RenderToMemoryPng()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ioutil.WriteFile("mapnik.png", blob, 0644)
+}
+
+func main() {
+	SimpleExample("demo/population.xml")
+}
--- maptiles/tileserver_postgres.go
+++ maptiles/tileserver_postgres.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"regexp"
 	"strconv"
-	// "strings"
 	"encoding/json"
 	"time"
 	"runtime"
@@ -15,41 +14,40 @@ import (
 
 // Handles HTTP requests for map tiles, caching any produced tiles
 // in an MBtiles 1.2 compatible sqlite db.
-type TileServer struct {
-	m         *TileDb
+type TileServerPostgres struct {
+	engine string
+	m         *TileDbPostgresql
 	lmp       *LayerMultiplex
 	TmsSchema bool
 	startTime time.Time
 }
 
-func NewTileServer(cacheFile string) *TileServer {
-	t := TileServer{}
+func NewTileServerPostgres(cacheFile string) *TileServerPostgres {
+	t := TileServerPostgres{}
 	t.lmp = NewLayerMultiplex()
-	t.m = NewTileDb(cacheFile)
+	// t.m = NewTileDbSqlite(cacheFile)
+	t.m = NewTileDbPostgresql(cacheFile)
 	t.startTime = time.Now()
-
 	return &t
 }
 
-func (t *TileServer) AddMapnikLayer(layerName string, stylesheet string) {
-	t.lmp.AddRenderer(layerName, stylesheet)
+func (self *TileServerPostgres) AddMapnikLayer(layerName string, stylesheet string) {
+	self.lmp.AddRenderer(layerName, stylesheet)
 }
 
-var pathRegex = regexp.MustCompile(`/([A-Za-z0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)\.png`)
-
-func (t *TileServer) ServeTileRequest(w http.ResponseWriter, r *http.Request, tc TileCoord) {
+func (self *TileServerPostgres) ServeTileRequest(w http.ResponseWriter, r *http.Request, tc TileCoord) {
 	// log.Println(r.RemoteAddr, tc)
 	ch := make(chan TileFetchResult)
 
 	tr := TileFetchRequest{tc, ch}
-	t.m.RequestQueue() <- tr
+	self.m.RequestQueue() <- tr
 
 	result := <-ch
 	needsInsert := false
 
 	if result.BlobPNG == nil {
 		// Tile was not provided by DB, so submit the tile request to the renderer
-		t.lmp.SubmitRequest(tr)
+		self.lmp.SubmitRequest(tr)
 		result = <-ch
 		if result.BlobPNG == nil {
 			// The tile could not be rendered, now we need to bail out.
@@ -65,31 +63,32 @@ func (t *TileServer) ServeTileRequest(w http.ResponseWriter, r *http.Request, tc
 		log.Println(err)
 	}
 	if needsInsert {
-		t.m.InsertQueue() <- result // insert newly rendered tile into cache db
+		self.m.InsertQueue() <- result // insert newly rendered tile into cache db
 	}
 }
 
-func (t *TileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (self *TileServerPostgres) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	log.Println(r.RemoteAddr, r.URL.Path)
 
 	if "/" == r.URL.Path {
-		t.IndexHandler(w, r)
+		self.IndexHandler(w, r)
 		return
 	} else if "/ping" == r.URL.Path {
-		t.PingHandler(w, r)
+		self.PingHandler(w, r)
 		return
 	} else if "/server" == r.URL.Path {
-		t.ServerHandler(w, r)
+		self.ServerHandler(w, r)
 		return
 	} else if "/metadata" == r.URL.Path {
-		t.MetadataHandler(w, r)
+		self.MetadataHandler(w, r)
 		return
 	}else if "/tilelayers" == r.URL.Path {
-		t.TileLayersHandler(w, r)
+		self.TileLayersHandler(w, r)
 		return
 	}
 
+	var pathRegex = regexp.MustCompile(`/([A-Za-z0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)\.png`)
 	path := pathRegex.FindStringSubmatch(r.URL.Path)
 
 	if path == nil {
@@ -102,11 +101,11 @@ func (t *TileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	x, _ := strconv.ParseUint(path[3], 10, 64)
 	y, _ := strconv.ParseUint(path[4], 10, 64)
 
-	t.ServeTileRequest(w, r, TileCoord{x, y, z, t.TmsSchema, l})
+	self.ServeTileRequest(w, r, TileCoord{x, y, z, self.TmsSchema, l})
 }
 
 
-func (t *TileServer) IndexHandler(w http.ResponseWriter, r *http.Request) {
+func (self *TileServerPostgres) IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	response := `{"status":"ok","data":{"message":"Hello there ladies and gentlemen!"}}`
@@ -118,9 +117,9 @@ func (t *TileServer) IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
-func (t *TileServer) MetadataHandler(w http.ResponseWriter, r *http.Request) {
+func (self *TileServerPostgres) MetadataHandler(w http.ResponseWriter, r *http.Request) {
 	// todo: include layer
-	metadata := t.m.MetaDataHandler()
+	metadata := self.m.MetaDataHandler()
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	response := make(map[string]interface{})
@@ -135,7 +134,7 @@ func (t *TileServer) MetadataHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // PingHandler provides an api route for server health check
-func (t *TileServer) PingHandler(w http.ResponseWriter, r *http.Request) {
+func (self *TileServerPostgres) PingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	data := `{"status": "success", "data": {"result": "pong"}}`
@@ -148,13 +147,13 @@ func (t *TileServer) PingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // ServerProfile returns basic server stats
-func (t *TileServer) ServerHandler(w http.ResponseWriter, r *http.Request) {
+func (self *TileServerPostgres) ServerHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	var data map[string]interface{}
 	data = make(map[string]interface{})
-	data["registered"] = t.startTime.UTC()
-	data["uptime"] = time.Since(t.startTime).Seconds()
+	data["registered"] = self.startTime.UTC()
+	data["uptime"] = time.Since(self.startTime).Seconds()
 	data["num_cores"] = runtime.NumCPU()
 	response := make(map[string]interface{})
 	response["status"] = "ok"
@@ -169,11 +168,11 @@ func (t *TileServer) ServerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func (t *TileServer) TileLayersHandler(w http.ResponseWriter, r *http.Request) {
+func (self *TileServerPostgres) TileLayersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	var keys []string
-	for k := range t.lmp.layerChans {
+	for k := range self.lmp.layerChans {
 		keys = append(keys,k)
 	}
 	var response map[string]interface{}
