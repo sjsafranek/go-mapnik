0a059e230e483fb973400b0009d89c2dfc2fbd9e
Stefan Safranek <stefan@stefan>
2017-03-30 11:56:34 -0700

    vagrant file

--- psql_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# sudo -i -u postgres
-psql -c "CREATE USER testuser WITH PASSWORD 'dev'"
-psql -c "CREATE DATABASE testuserdb"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE testuserdb TO testuser"
